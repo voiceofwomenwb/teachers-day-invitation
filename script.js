6312c08e37798b3009e487ff501e6b321fee4bcc
voiceofwomenwb <wbengal2026@gmail.com>
2026-07-25 08:24:36 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,13 +2,38 @@ const loader = document.getElementById("loader");
 const main = document.getElementById("main");
 const envelope = document.querySelector(".envelope");
 const btn = document.getElementById("openBtn");
+const seal = document.getElementById("seal");
+const instruction = document.getElementById("instruction");
 
 setTimeout(() => {
     loader.style.display = "none";
     main.style.display = "flex";
 }, 3000);
 
+// Step 1
 btn.addEventListener("click", () => {
-    envelope.classList.add("open");
+
     btn.style.display = "none";
+
+    instruction.style.display = "block";
+
+    seal.classList.add("glow");
+
+});
+
+// Step 2
+seal.addEventListener("click", () => {
+
+    seal.classList.remove("glow");
+
+    seal.style.transform = "translateX(-50%) scale(0.92)";
+
+    setTimeout(() => {
+
+        seal.style.transform = "translateX(-50%) scale(1)";
+
+        envelope.classList.add("open");
+
+    },200);
+
 });
